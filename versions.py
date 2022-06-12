9b4c151f88116c22c66debb9f6e70ef05ec61066
Samridh29 <samridhagarwal011@gmail.com>
2022-06-12 23:42:54 +0530

    Final

---
+++ versions.py
@@ -0,0 +1,13 @@
+import sys
+import scipy
+import numpy
+import matplotlib
+import sklearn
+import pandas
+
+
+print(scipy.__version__)
+print(numpy.__version__)
+print(matplotlib.__version__)
+print(sklearn.__version__)
+print(pandas.__version__)
